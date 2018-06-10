5ad8c18876750947e1fa065302224da105e3bf1a
Anthony Elijah <lionnet.ant@gmail.com>
2018-06-10 11:06:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,12 +18,15 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 
-// Return Grandma sentence to lowercase argument
+// Return Grandma sentences to argument
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) {
     return 'I can\'t hear you!'
   }
   else if (string === string.toUpperCase()) {
     return 'YES INDEED!'
+  }
+  else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
   }
 }
