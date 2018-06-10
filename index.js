c218bb46689604c56c9aa99a6fb6b3807396bfda
Anthony Elijah <lionnet.ant@gmail.com>
2018-06-10 10:55:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+// Return an argument in uppercase
+function shout(string) {
+  return string.toUpperCase()
+}
+
+// Return an argument in lowercase
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+// Print an argument in uppercase
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+// Print an argument in lowercase
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+// Return Grandma sentence to hi
+function sayHiToGrandma(string) {
+  var lowercase = string
+  if (string.toLowerCase === lowercase) {
+    return 'I can\'t hear you!'
+  }
+}
